0c4a903e7a7fd5e9bb040600fe62f0504e55748b
Declan Noetzelman <declan@noetzelman.com>
2026-07-04 15:31:55 -0400

    feat(character repository): create character repository
    This repository provides the database interactions necessary for the Character table

--- RPG.Core/Interfaces/Repositories/ICharacterRepository.cs
+++ RPG.Core/Interfaces/Repositories/ICharacterRepository.cs
@@ -5,8 +5,8 @@ namespace RPG.Core.Interfaces.Repositories;
 public interface ICharacterRepository
 {
     Task<Character?> GetByIdAsync(int id);
-    Task<IEnumerable<Character>> GetAllAsync();
-    Task<IEnumerable<Character>> GetByCampaignIdAsync();
+    Task<IEnumerable<Character>> GetAllByUserAsync(int userId);
+    Task<IEnumerable<Character>> GetByCampaignIdAsync(int campaignId);
     Task AddAsync(Character character);
     Task UpdateAsync(Character character);
     Task DeleteAsync(int id);
---
+++ RPG.Infrastructure/Repositories/CharacterRepository.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using RPG.Core.Entities.Characters;
+using RPG.Core.Interfaces.Repositories;
+using RPG.Infrastructure.Data;
+
+namespace RPG.Infrastructure.Repositories;
+
+public class CharacterRepository : ICharacterRepository
+{
+    private readonly AppDbContext _context;
+
+    public CharacterRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Character?> GetByIdAsync(int id)
+    {
+        return await _context.Characters
+            .Include(c => c.Player)
+            .Include(c => c.Campaign)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<IEnumerable<Character>> GetAllByUserAsync(int userId)
+    {
+        return await _context.Characters
+            .Where(c => c.Player.Id == userId)    
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Character>> GetByCampaignIdAsync(int campaignId)
+    {
+        return await _context.Characters
+            .Where(c => c.CampaignId == campaignId)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Character character)
+    {
+        await _context.Characters.AddAsync(character);
+    }
+
+    public Task UpdateAsync(Character character)
+    {
+        _context.Characters.Update(character);
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var character = await _context.Characters.FindAsync(id);
+        if (character != null)
+        {
+            _context.Characters.Remove(character);
+        }
+    }
+}
