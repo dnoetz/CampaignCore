fbd3589e4ed314d02d4d4375fd06fe47171edd69
Declan Noetzelman <declan@noetzelman.com>
2026-07-04 21:45:51 -0400

    feat(user repository): create user repository
    This repository provides database interactions necessary for the Users table

--- RPG.Core/Interfaces/Repositories/IUserRepository.cs
+++ RPG.Core/Interfaces/Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace RPG.Core.Interfaces.Repositories;
 
 public interface IUserRepository
 {
-    Task<User> GetByIdAsync(int id);
+    Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
     Task AddAsync(User user);
     Task UpdateAsync(User user);
---
+++ RPG.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using RPG.Core.Entities;
+using RPG.Core.Interfaces.Repositories;
+using RPG.Infrastructure.Data;
+
+namespace RPG.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        _context.Users.Update(user);
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+        }
+    }
+}
