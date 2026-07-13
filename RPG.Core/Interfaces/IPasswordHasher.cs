41944949049bbc434998121fd75118275b2c6e5a
Declan Noetzelman <declan@noetzelman.com>
2026-07-12 22:18:29 -0400

    feat(password encryption): add password encryption via BCrypt.Net-Next and implement hasher interface

---
+++ RPG.Core/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+namespace RPG.Core.Interfaces;
+
+public interface IPasswordHasher
+{
+     string HashPassword(string password);
+     bool VerifyPassword(string password, string hashedPassword);
+}
