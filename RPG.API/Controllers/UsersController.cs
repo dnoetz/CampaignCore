e942e3a035cb40f715488a4d7b78bb75953f054c
Declan Noetzelman <declan@noetzelman.com>
2026-07-13 21:28:38 -0400

    feat(users controller): add user controller with RegisterUserAsync and UpdateUserAsync

--- RPG.API/Controllers/UsersController.cs
+++ RPG.API/Controllers/UsersController.cs
@@ -1,6 +1,45 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RPG.API.DTOs.User;
+using RPG.Core.Services;
+
 namespace RPG.API.Controllers;
 
-public class UsersController
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    
+    private readonly UserService _userService;
+
+    public UsersController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register-user")]
+    public async Task<ActionResult> RegisterUserAsync(CreateUserRequestDto newUser)
+    {
+        await _userService.CreateUser(newUser.Username, newUser.FirstName, newUser.LastName, newUser.Password,
+            newUser.Email, newUser.Birthday);
+        return Created();
+    }
+
+    [Authorize]
+    [HttpPut("update-user")]
+    public async Task<ActionResult> UpdateUserAsync(UserUpdateDto userUpdate)
+    {
+        var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        
+        if (userIdClaim == null || !int.TryParse(userIdClaim, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        await _userService.UpdateUserAsync(userId, userUpdate.Username, userUpdate.Email);
+
+        return Ok();
+    }
 }
