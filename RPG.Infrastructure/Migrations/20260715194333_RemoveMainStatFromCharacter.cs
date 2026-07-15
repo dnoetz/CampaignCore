dab7e7f70542b4ee708206eae58c8579ad65cb18
Declan Noetzelman <declan@noetzelman.com>
2026-07-15 15:47:24 -0400

    chore(migrate character changes): migrate main stat drop from character into characters table

---
+++ RPG.Infrastructure/Migrations/20260715194333_RemoveMainStatFromCharacter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RPG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMainStatFromCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainStat",
+                table: "Characters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MainStat",
+                table: "Characters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
