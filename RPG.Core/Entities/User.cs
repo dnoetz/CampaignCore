8b2b76ac888995dfbf32bb27c59ee8a3074e6936
Declan Noetzelman <declan@noetzelman.com>
2026-07-17 21:56:05 -0400

    feat(unit testing): add combat service unit testing suite
    This suite tests combat execution: player dealing damage when initiative is greater than 5, player receiving damage when initiative is less than 5, dealing critical damage when roll is 6, and player receiving exp upon enemy death

--- RPG.Core/Entities/User.cs
+++ RPG.Core/Entities/User.cs
@@ -25,5 +25,6 @@ public class User
         Email = email;
         Birthday = birthday;
         OwnedCampaigns = [];
+        OwnedCharacters = [];
     }
 }
--- RPG.Tests/Services/CombatServiceTests.cs
+++ RPG.Tests/Services/CombatServiceTests.cs
@@ -1,40 +1,104 @@
 using Moq;
-using RPG.Core.Entities.Characters.Necromancer;
+using RPG.Core.Entities.Campaigns;
+using RPG.Core.Entities.Characters;
 using RPG.Core.Entities.Monsters;
+using RPG.Core.Enums;
+using RPG.Core.Interfaces;
+using RPG.Core.Interfaces.Repositories;
+using RPG.Core.Interfaces.Services;
 using RPG.Core.Services;
-
-using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace RPG.Tests.Services;
 
 public class CombatServiceTests
 {
-    /*
-    [Fact]
-    public void ExecuteCombatTurn_PlayerAbilities_AreSearchable()
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly Character _necro = new("Necro", 100, 1, 11, 1, 1, 1, PlayableClasses.Necromancer);
+    private readonly Monster _enemy = new Goblin("Goblin");
+    private readonly CombatService _sut;
+    private readonly Mock<IUnitOfWork> _mockUoW;
+    private readonly Mock<ICharacterRepository> _mockCharacterRepository;
+    private readonly Mock<IDamageCalculatorService> _mockDamageCalculatorService;
+    private readonly Mock<IExperienceService> _mockExperienceService;
+    private readonly Mock<IActionLoggerService> _mockActionLoggerService;
+
+    public CombatServiceTests(ITestOutputHelper testOutputHelper)
     {
-        var combat = new CombatService();
-        var player = new PlayerNecromancer("Necro");
-        var enemy = new Goblin("Goblin");
+        _testOutputHelper = testOutputHelper;
+        _mockUoW = new Mock<IUnitOfWork>();
         
-        player.RaiseUndead();
+        _mockActionLoggerService = new Mock<IActionLoggerService>(MockBehavior.Strict);
+        _mockActionLoggerService
+            .Setup(l => l.LogAction(It.IsAny<string>(), It.IsAny<Character>(), It.IsAny<ActionType>(),
+                It.IsAny<string>()))
+            .ReturnsAsync(new CampaignAction()
+            {
+                ActionType = ActionType.Combat,
+                Actor = _necro,
+                Narrative = "Necromancer attacks Goblin",
+                Result = "Test",
+                Timestamp = DateTime.UtcNow,
+                CampaignId = 1
+            });
         
-        combat.ExecuteCombatTurn(player, enemy, "Necrosis", 15, 4);
+        _mockExperienceService = new Mock<IExperienceService>();
+        
+        _mockDamageCalculatorService = new Mock<IDamageCalculatorService>(MockBehavior.Strict);
+        _mockDamageCalculatorService
+            .Setup(d => d.CalculateDamage(It.IsAny<Character>(), It.IsAny<int>(), It.IsAny<string>()))
+            .Returns(30);
+        _mockDamageCalculatorService
+            .Setup(d => d.CalculateCriticalDamage(It.IsAny<Character>(),  It.IsAny<string>()))
+            .Returns(39);
+        
+        _mockCharacterRepository = new Mock<ICharacterRepository>(MockBehavior.Strict);
+        _mockCharacterRepository
+            .Setup(r => r.GetByIdAsync(_necro.Id))
+            .ReturnsAsync(_necro);
 
-        var enemyHealthAfterNecrosis = enemy.CurrentHitpoints;
-        var actualDamageNecrosis = enemy.MaxHitpoints - enemyHealthAfterNecrosis;
-        var expectedDamageNecrosis = player.DealDamage("Necrosis") + 4;
+        _mockCharacterRepository
+            .Setup(r => r.UpdateAsync(It.IsAny<Character>()))
+            .Returns(Task.CompletedTask);
         
-        Assert.True(enemy.CurrentHitpoints < enemy.MaxHitpoints);
-        Assert.Equal(expectedDamageNecrosis, actualDamageNecrosis);
-        
-        combat.ExecuteCombatTurn(player, enemy, "ReapersMark", 15, 4);
-
-        var actualDamageReapersMark = enemyHealthAfterNecrosis - enemy.CurrentHitpoints;
-        var expectedDamageReapersMark = player.DealDamage("ReapersMark") + 4;
-        
-        Assert.True(enemy.CurrentHitpoints < enemyHealthAfterNecrosis);
-        Assert.Equal(expectedDamageReapersMark, actualDamageReapersMark);
+        _sut = new CombatService(_mockDamageCalculatorService.Object, _mockExperienceService.Object,
+            _mockCharacterRepository.Object, _mockUoW.Object, _mockActionLoggerService.Object);
     }
-    */
+    
+    [Fact]
+    public async Task ExecuteCombatTurn_PlayerDealsDamage_WhenInitiativeIsGreaterThan5()
+    {
+        await _sut.ExecuteCombatTurn(_necro, _enemy, "Necrosis", 15, 4, "Necromancer attacks Goblin");
+
+        Assert.True(_enemy.CurrentHitpoints < _enemy.MaxHitpoints);
+    }
+    
+    [Fact]
+    public async Task ExecuteCombatTurn_PlayerReceivesDamage_WhenInitiativeIsLessThan5()
+    {
+        await _sut.ExecuteCombatTurn(_necro, _enemy, "Necrosis", 3, 4, "Necromancer attacks Goblin");
+
+        Assert.True(_necro.CurrentHitpoints < _necro.MaxHitpoints);
+    }
+    
+    [Fact]
+    public async Task ExecuteCombatTurn_PlayerDamageIsCritical_WhenRollIs6()
+    {
+        await _sut.ExecuteCombatTurn(_necro, _enemy, "Necrosis", 15, 6, "Necromancer attacks Goblin");
+
+        Assert.True(_enemy.CurrentHitpoints < _enemy.MaxHitpoints);
+        Assert.Equal((_enemy.MaxHitpoints - 39), _enemy.CurrentHitpoints);
+    }
+
+    [Fact]
+    public async Task ExecuteCombatTurn_AwardsExp_OnEnemyDeath()
+    {
+        while (!_enemy.IsDead)
+        {
+            await _sut.ExecuteCombatTurn(_necro, _enemy, "Necrosis", 15, 6, "Necromancer attacks Goblin");
+        }
+        
+        _mockExperienceService.Verify(e => e.AwardExp(_necro, _enemy), Times.Once());
+    }
 }
