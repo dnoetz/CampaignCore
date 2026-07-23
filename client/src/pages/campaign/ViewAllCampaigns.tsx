0739c3b6f9793f0a9ca2f528453d14dea1fe5f05
Declan Noetzelman <declan@noetzelman.com>
2026-07-22 23:44:08 -0400

    feat(all campaigns view): add all campaigns view
    Each campaign element rendered is a clickable link which will navigate to the specific user campaign

---
+++ client/src/pages/campaign/ViewAllCampaigns.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import { getAllCampaigns } from '../../api/campaign.ts'
+import CampaignSummaryCard from '../../components/CampaignSummaryCard.tsx'
+function ViewAllCampaigns() {
+    interface Campaign {
+        id: number
+        name: string
+        campaignCode: string
+    }
+
+    const [campaigns, setCampaigns] = useState<Campaign[]>([])
+
+    useEffect(() => {
+        async function load () {
+            const campaigns = await getAllCampaigns()
+            setCampaigns(campaigns)
+        }
+        load();
+    }, []);
+    
+    return(
+        <div className={"p-4 flex flex-col h-dvh justify-center"}>
+            <div className={"p-4 gap-4 flex flex-col"}>
+                <h1 className={"text-5xl text-center"}>Campaign List</h1>
+                <div className={"flex flex-col gap-2 p-4 border-2 rounded-lg border-gray-500/50"}>
+                    <p className={"flex flex-row justify-between font-bold text-xl"}><span>Campaign Name</span><span>Share Code</span></p>
+                    <div className={"flex flex-col gap-2"}>
+                        {campaigns.map(campaign => <CampaignSummaryCard campaign={campaign} key={campaign.id}/>)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAllCampaigns
